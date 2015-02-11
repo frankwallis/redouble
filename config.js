a2f8732eb1a995fcd87ca19aec6c75e048d9dcf3
Frank Wallis <fwallis@outlook.com>
2015-02-11 19:47:19 +0000

    fix(systemjs-config): remove unnecessary config

--- config.js
+++ config.js
@@ -2,16 +2,8 @@ System.config({
   "paths": {
     "*": "./",
     "src/*": "src/*",
-    "src/model/gameplay-async/index.js": "src/model/gameplay-async/index.js",
-    "src/model/gameplay-async/rubber.js": "src/model/gameplay-async/rubber.js",
-    "src/model/gameplay-async/board.js": "src/model/gameplay-async/board.js",
-    "src/model/gameplay-async/bidding.js": "src/model/gameplay-async/bidding.js",
-    "src/model/gameplay-async/cardplay.js": "src/model/gameplay-async/cardplay.js",
-    "src/model/gameplay-async/utils.js": "src/model/gameplay-async/utils.js",
-    "src/model/gameplay-async/*": "src/model/gameplay-async/*.js",
     "github:*": "jspm_packages/github/*.js",
     "npm:*": "jspm_packages/npm/*.js",
-    "typescript/bin/*.d.ts": "jspm_packages/npm/typescript@1.4.1/bin/*.d.ts",
     "tower/*": "src/*.js"
   },
   "traceurOptions": {
@@ -26,40 +18,56 @@ System.config({
 
 System.config({
   "map": {
-    "angular": "github:angular/bower-angular@1.3.10",
-    "angular-material": "github:angular/bower-material@0.7.0",
+    "ts": "github:frankwallis/plugin-typescript@0.5.7",
+    "css": "github:systemjs/plugin-css@0.1.5",
+    "html": "github:systemjs/plugin-text@0.0.2",
+    "angular": "github:angular/bower-angular@1.3.13",
+    "angular-material": "github:angular/bower-material@0.7.1",
     "angular-ui-router": "github:angular-ui/ui-router@0.2.13",
     "hammer": "github:hammerjs/hammer.js@2.0.4",
-    "html": "github:systemjs/plugin-text@0.0.2",
-    "css": "github:systemjs/plugin-css@0.1.0",
-    "plugin-css": "github:systemjs/plugin-css@0.1.0",
+    "plugin-css": "github:systemjs/plugin-css@0.1.5",
     "plugin-text": "github:systemjs/plugin-text@0.0.2",
-    "plugin-typescript@0.5.5": "github:frankwallis/plugin-typescript@0.5.5",
-    "ts": "github:frankwallis/plugin-typescript@0.5.5",
+    "plugin-typescript": "github:frankwallis/plugin-typescript@0.5.7",
     "github:angular-ui/ui-router@0.2.13": {
-      "angular": "github:angular/bower-angular@1.3.10"
+      "angular": "github:angular/bower-angular@1.3.13"
     },
-    "github:angular/bower-angular-animate@1.3.10": {
-      "angular": "github:angular/bower-angular@1.3.10"
+    "github:angular/bower-angular-animate@1.3.13": {
+      "angular": "github:angular/bower-angular@1.3.13"
     },
-    "github:angular/bower-angular-aria@1.3.10": {
-      "angular": "github:angular/bower-angular@1.3.10"
+    "github:angular/bower-angular-aria@1.3.13": {
+      "angular": "github:angular/bower-angular@1.3.13"
     },
-    "github:angular/bower-material@0.7.0": {
-      "angular": "github:angular/bower-angular@1.3.10",
-      "angular-animate": "github:angular/bower-angular-animate@1.3.10",
-      "angular-aria": "github:angular/bower-angular-aria@1.3.10",
-      "css": "github:systemjs/plugin-css@0.1.0",
-      "hammer": "github:hammerjs/hammer.js@2.0.4"
+    "github:angular/bower-material@0.7.1": {
+      "angular": "github:angular/bower-angular@1.3.13",
+      "angular-animate": "github:angular/bower-angular-animate@1.3.13",
+      "angular-aria": "github:angular/bower-angular-aria@1.3.13",
+      "css": "github:systemjs/plugin-css@0.1.5"
     },
-    "github:frankwallis/plugin-typescript@0.5.5": {
+    "github:frankwallis/plugin-typescript@0.5.7": {
       "convert-source-map": "npm:convert-source-map@0.4.1",
       "path": "npm:path@0.11.14",
       "requires": "npm:requires@1.0.2",
       "typescript": "npm:typescript@1.4.1"
     },
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
+    },
     "github:jspm/nodelibs-buffer@0.1.0": {
       "buffer": "npm:buffer@3.0.1"
+    },
+    "github:jspm/nodelibs-events@0.1.0": {
+      "events-browserify": "npm:events-browserify@0.0.1"
+    },
+    "github:jspm/nodelibs-http@1.7.0": {
+      "Base64": "npm:Base64@0.2.1",
+      "events": "github:jspm/nodelibs-events@0.1.0",
+      "inherits": "npm:inherits@2.0.1",
+      "stream": "github:jspm/nodelibs-stream@0.1.0",
+      "url": "github:jspm/nodelibs-url@0.1.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "github:jspm/nodelibs-https@0.1.0": {
+      "https-browserify": "npm:https-browserify@0.0.0"
     },
     "github:jspm/nodelibs-os@0.1.0": {
       "os-browserify": "npm:os-browserify@0.1.2"
@@ -70,7 +78,29 @@ System.config({
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.0"
     },
+    "github:jspm/nodelibs-querystring@0.1.0": {
+      "querystring": "npm:querystring@0.2.0"
+    },
+    "github:jspm/nodelibs-stream@0.1.0": {
+      "stream-browserify": "npm:stream-browserify@1.0.0"
+    },
+    "github:jspm/nodelibs-url@0.1.0": {
+      "url": "npm:url@0.10.2"
+    },
     "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "github:systemjs/plugin-css@0.1.5": {
+      "clean-css": "npm:clean-css@3.0.10",
+      "fs": "github:jspm/nodelibs-fs@0.1.1"
+    },
+    "npm:amdefine@0.1.0": {
+      "fs": "github:jspm/nodelibs-fs@0.1.1",
+      "module": "github:jspm/nodelibs-module@0.1.0",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
     "npm:buffer@3.0.1": {
@@ -78,10 +108,38 @@ System.config({
       "ieee754": "npm:ieee754@1.1.4",
       "is-array": "npm:is-array@1.0.1"
     },
+    "npm:clean-css@3.0.10": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "commander": "npm:commander@2.5.1",
+      "fs": "github:jspm/nodelibs-fs@0.1.1",
+      "http": "github:jspm/nodelibs-http@1.7.0",
+      "https": "github:jspm/nodelibs-https@0.1.0",
+      "os": "github:jspm/nodelibs-os@0.1.0",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "source-map": "npm:source-map@0.1.43",
+      "url": "github:jspm/nodelibs-url@0.1.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:commander@2.5.1": {
+      "child_process": "github:jspm/nodelibs-child_process@0.1.0",
+      "events": "github:jspm/nodelibs-events@0.1.0",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
     "npm:convert-source-map@0.4.1": {
       "buffer": "github:jspm/nodelibs-buffer@0.1.0",
       "fs": "github:jspm/nodelibs-fs@0.1.1",
       "path": "github:jspm/nodelibs-path@0.1.0"
+    },
+    "npm:core-util-is@1.0.1": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0"
+    },
+    "npm:events-browserify@0.0.1": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:https-browserify@0.0.0": {
+      "http": "github:jspm/nodelibs-http@1.7.0"
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
@@ -97,11 +155,45 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1",
       "util": "github:jspm/nodelibs-util@0.1.0"
     },
+    "npm:punycode@1.3.2": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:readable-stream@1.1.13": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0",
+      "core-util-is": "npm:core-util-is@1.0.1",
+      "events": "github:jspm/nodelibs-events@0.1.0",
+      "inherits": "npm:inherits@2.0.1",
+      "isarray": "npm:isarray@0.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "stream": "npm:stream-browserify@1.0.0",
+      "string_decoder": "npm:string_decoder@0.10.31",
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:source-map@0.1.43": {
+      "amdefine": "npm:amdefine@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.1",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:stream-browserify@1.0.0": {
+      "events": "github:jspm/nodelibs-events@0.1.0",
+      "inherits": "npm:inherits@2.0.1",
+      "readable-stream": "npm:readable-stream@1.1.13"
+    },
+    "npm:string_decoder@0.10.31": {
+      "buffer": "github:jspm/nodelibs-buffer@0.1.0"
+    },
     "npm:typescript@1.4.1": {
       "fs": "github:jspm/nodelibs-fs@0.1.1",
       "os": "github:jspm/nodelibs-os@0.1.0",
       "path": "github:jspm/nodelibs-path@0.1.0",
       "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:url@0.10.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "punycode": "npm:punycode@1.3.2",
+      "querystring": "github:jspm/nodelibs-querystring@0.1.0",
+      "util": "github:jspm/nodelibs-util@0.1.0"
     },
     "npm:util@0.10.3": {
       "inherits": "npm:inherits@2.0.1",
@@ -109,4 +201,3 @@ System.config({
     }
   }
 });
-

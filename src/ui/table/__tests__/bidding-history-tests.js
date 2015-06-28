8d07ac6976ede971c8d4f6df1e0f5b3d4ac1f7c2
Frank Wallis <fwallis@outlook.com>
2015-06-28 22:02:08 +0100

    test(bidding-history): fix failing tests

--- src/ui/table/__tests__/bidding-history-tests.js
+++ src/ui/table/__tests__/bidding-history-tests.js
@@ -2,32 +2,33 @@ jest.autoMockOff()
 
 import {BidComponent} from  "../../components/bid.jsx";
 import {BiddingHistory} from "../bidding-history.jsx";
-import {Game} from "../../../model/game/game-state";
+import {Board} from "../../../model/game/board-state";
 import {Bid, BidType, BidSuit} from "../../../model/core/bid";
+import {Seat} from "../../../model/core/seat";
 
 import React from "react/addons";
 let TestUtils = React.addons.TestUtils;
 
 describe('Bidding History', () => {
 
-  it('displays the right headings', () => {
-      let game = new Game().newBoard();
-      let biddingHistory = TestUtils.renderIntoDocument(<BiddingHistory board={game.currentBoard}/>);
+   it('displays the right headings', () => {
+      let board = new Board({ dealer: Seat.West, bids: [], cards: [] })
+      let biddingHistory = TestUtils.renderIntoDocument(<BiddingHistory board={board}/>);
       let thead = TestUtils.findRenderedDOMComponentWithTag(biddingHistory, 'thead');
       let headings = TestUtils.scryRenderedDOMComponentsWithTag(thead, 'th');
 
       expect(headings.length).toEqual(4);
       expect(headings[0].getDOMNode().textContent).toBe('north');
-  });
+   });
 
-  it('displays all the bids', () => {
-      let game = new Game().newBoard();
-      game.currentBoard.bids.push({ type: BidType.NoBid });
+   it('displays all the bids', () => {
+      let board = new Board({ dealer: Seat.West, bids: [], cards: [] })
+         .makeBid(Bid.create("no bid"))
 
-      let biddingHistory = TestUtils.renderIntoDocument(<BiddingHistory board={game.currentBoard}/>);
+      let biddingHistory = TestUtils.renderIntoDocument(<BiddingHistory board={board}/>);
       let tbody = TestUtils.findRenderedDOMComponentWithTag(biddingHistory, 'tbody');
 
-      bids = TestUtils.scryRenderedComponentsWithType(tbody, BidComponent);
+      let bids = TestUtils.scryRenderedComponentsWithType(tbody, BidComponent);
       expect(bids.length).toEqual(1);
   });
 });

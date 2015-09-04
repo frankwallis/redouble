9224839bd861bab0f39b3374e1fe35ff4c39623e
Frank Wallis <fwallis@outlook.com>
2015-09-05 00:08:32 +0100

    chore(dev-tools): add redux-dev-tools

--- src/ui/client.jsx
+++ src/ui/client.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import {createStore, applyMiddleware, combineReducers} from 'redux';
+// Redux
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
 import reducers from '../stores/index';
 
+// Redux DevTools store enhancers and components
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
+// React Router
 import {Router} from 'react-router';
 import {history} from 'react-router/lib/BrowserHistory';
 import routes from "./routes";
@@ -17,13 +23,21 @@ import 'font-awesome/css/font-awesome.css';
 console.log("Received state " + JSON.stringify(global.__ISOMORPHIC_STATE__));
 
 const reducer = combineReducers(reducers);
-const finalCreateStore = applyMiddleware(thunk)(createStore);
+const finalCreateStore = compose(applyMiddleware(thunk),
+	devTools(),
+	// Lets you write ?debug_session=<name> in address bar to persist debug sessions
+	persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)))(createStore);
 const store = finalCreateStore(reducer, global.__ISOMORPHIC_STATE__);
 
 ReactDOM.render(
-	<Provider store={store}>
-		<Router history={history}>
-			{routes}
-		</Router>
-	</Provider>,
+	<div>
+		<Provider store={store}>
+			<Router history={history}>
+				{routes}
+			</Router>
+		</Provider>
+		<DebugPanel top right bottom>
+			<DevTools store={store} monitor={LogMonitor} />
+		</DebugPanel>
+	</div>,
 	document.getElementById('main'));

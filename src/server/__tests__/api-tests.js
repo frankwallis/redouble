01b41cbc5d1c20523f0a769acdbccc9d5e5dbaa0
Frank Wallis <fwallis@outlook.com>
2015-10-03 09:49:30 +0100

    test(errors): add test for error handling

--- src/server/__tests__/api-tests.js
+++ src/server/__tests__/api-tests.js
@@ -30,7 +30,7 @@ describe('API Integration Tests', () => {
 	it('getCard', (done) => {
 		let gameBuilder = GameBuilder.create().newBoard(
 			Seat.West,
-			Deck.fromPBN("N: 2...A2 7.7..7 A.A..3 456..."),
+			Deck.fromPBN("N:2...A2 7.7..7 A.A..3 456..."),
 			Bid.createAll("no bid", "no bid", "no bid", "1NT", "no bid", "no bid", "no bid")
 		);
 
@@ -45,13 +45,28 @@ describe('API Integration Tests', () => {
 	it('getBid', (done) => {
 		let gameBuilder = GameBuilder.create().newBoard(
 			Seat.North,
-			Deck.fromPBN("N: AKQ63.A97.72.T92 J52.KJT2.J.AQ743 84.865.QT85.KJ86 T97.Q43.AK9643.5")
+			Deck.fromPBN("N:AKQ63.A97.72.T92 J52.KJT2.J.AQ743 84.865.QT85.KJ86 T97.Q43.AK9643.5")
 		);
 
 		// single call with positional parameter
 		client.invoke('getBid', [ gameBuilder.build() ], function (err, raw) {
 			let obj = JSON.parse(raw);
 			expect(obj.result).to.deep.equal({"type": 2, "level": 1, "suit": 4});
+			done();
+		});
+	});
+
+	it('getBid errors', (done) => {
+		let gameBuilder = GameBuilder.create().newBoard(
+			Seat.North,
+			Deck.fromPBN("N:AAQ63.A97.72.T92 J52.KJT2.J.AQ743 84.865.QT85.KJ86 T97.Q43.AK9643.5")
+		);
+
+		// single call with positional parameter
+		client.invoke('getBid', [ gameBuilder.build() ], function (err, raw) {
+			let obj = JSON.parse(raw);
+			expect(obj.error).to.deep.equal({"code": -32099, "message": "Cannot read property 'type' of undefined"});
+			expect(obj.result).to.be.undefined;
 			done();
 		});
 	});
